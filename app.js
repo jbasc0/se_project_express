c9599bb0f05f893387d890a23aa8f14a80bedf5e
Jovito <jbb1117@yahoo.com>
2024-04-10 09:58:31 -0700

    user test

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
+const { middleware } = require("./middlewares/auth");
+
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
 app.use(cors());
 app.use("/", router);
+app.use((req, res, next) => {
+  req.user = {
+    _id: middleware,
+  };
+
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
